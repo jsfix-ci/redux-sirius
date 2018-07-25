f691e1f193263bdc32f717ae984ff96df7049f5e
Hetian Zhu <zhuht@yunphant.com>
2018-07-25 10:27:47 +0800

    initial commit

---
+++ src/core/index.js
@@ -0,0 +1,107 @@
+/* eslint-disable no-underscore-dangle,no-undef */
+import invariant from 'invariant'
+import { handleActions } from 'redux-actions'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+import * as sagaEffects from 'redux-saga/effects'
+
+const dash = '/'
+
+class Sirius {
+  constructor (options) {
+    this.options = options || {}
+    this._models = []
+  }
+
+  model (m) {
+    checkModel(m)
+    this._models.push(m)
+  }
+
+  models (...models) {
+    // TODO: directly read model from path
+    for (const m of models) {
+      this.model(m)
+    }
+  }
+
+  store () {
+    const models = this._models
+    const reducerObj = {}
+    const sagas = []
+    for (const model of models) {
+      // TODO: use file name as namespace
+      const handlers = {}
+      // generate default reducers by state
+      for (const key of Object.keys(model.state)) {
+        handlers[addSetPrefix(model.namespace)(key)] = (state, action) => ({ ...state, [key]: action.payload })
+      }
+      // add user defined reducers
+      if (model.reducers) {
+        for (const r of Object.keys(model.reducers)) {
+          handlers[addPrefix(model.namespace)(r)] = model.reducers[r]
+        }
+      }
+      reducerObj[model.namespace] = handleActions(handlers, model.state)
+      if (model.effects) {
+        for (const key of Object.keys(model.effects)) {
+          const sagaKey = addPrefix(key)
+          // FIXME: Only support takeEvery now
+          sagas.push(function * e () {
+            yield sagaEffects.fork(function * t () {
+              yield sagaEffects.takeEvery(sagaKey, model.effects[key])
+            })
+          })
+        }
+      }
+    }
+    let store
+    const sagaMiddleware = createSagaMiddleware()
+    const { middlewares } = this.options
+    let mws
+    if (!Array.isArray(middlewares)) {
+      mws = applyMiddleware(sagaMiddleware)
+    } else {
+      // TODO: custom middleware order support
+      mws = applyMiddleware(...middlewares, sagaMiddleware)
+    }
+    if (__DEV__) {
+      store = createStore(combineReducers(reducerObj),
+        // redux devtools support
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+        mws)
+    } else {
+      store = createStore(combineReducers(reducerObj), mws)
+    }
+    sagas.forEach(sagaMiddleware.run)
+    return store
+  }
+}
+function checkModel (model) {
+  invariant(model.namespace, '`namespace` must be set for a model')
+  invariant(typeof model.namespace === 'string', '`namespace` must be a string')
+  invariant(model.namespace.trim().length !== 0, `Invalid \`namespace\` : [${model.namespace}]`)
+  invariant(model.state, 'A model must have `state` field')
+  // TODO: check duplicate namespace
+  return model
+}
+
+function addSetPrefix (namespace) {
+  return name => (namespace ? `${namespace}${dash}set${upperCaseFirst(name)}`
+    : `set${upperCaseFirst(name)}`)
+}
+
+function addPrefix (namespace) {
+  return namespace ? name => `${namespace}${dash}${name}` : name => name
+}
+
+function upperCaseFirst (s) {
+  if (s.length > 1) {
+    return s[0].toUpperCase() + s.substring(1, s.length)
+  } else {
+    return s.toUpperCase()
+  }
+}
+
+exports.effects = sagaEffects
+export default Sirius
---
+++ src/core/index.test.js
@@ -0,0 +1,153 @@
+/* eslint-disable no-underscore-dangle */
+import Sirius, { effects } from './index'
+
+const delay = duration => new Promise(resolve => setTimeout(resolve, duration))
+// no namespace / no reducers / no effects
+const model0 = {
+  state: {
+    name: 'fuck',
+    value: 0,
+    lovers: [
+      {
+        name: 'qwe',
+        sex: 1
+      },
+      {
+        name: 'ewq',
+        sex: 0
+      }
+    ]
+  }
+}
+test('new Sirius with empty models', () => {
+  const s = new Sirius()
+  expect(s._models.length).toBe(0)
+})
+
+test('Model without namespace', () => {
+  const app = new Sirius()
+  try {
+    app.model(model0)
+  } catch (e) {
+    expect(e.message).toBe('`namespace` must be set for a model')
+  }
+})
+
+test('Model without reducers', () => {
+  const app = new Sirius()
+  app.model({ ...model0, namespace: 'test' })
+  const store = app.store()
+  const state = store.getState()
+  expect(state.test).toEqual({
+    name: 'fuck',
+    value: 0,
+    lovers: [
+      {
+        name: 'qwe',
+        sex: 1
+      },
+      {
+        name: 'ewq',
+        sex: 0
+      }
+    ]
+  })
+})
+
+test('Model default reducers', () => {
+  const app = new Sirius()
+  app.model({ ...model0, namespace: 'test' })
+  const store = app.store()
+  store.dispatch({
+    type: 'test/setName',
+    payload: '~~~'
+  })
+  const state = store.getState()
+  expect(state.test.name).toBe('~~~')
+})
+
+test('Model with customized reducers', () => {
+  const app = new Sirius()
+  app.model({
+    ...model0,
+    namespace: 'test',
+    reducers: {
+      setName: (state, { payload }) => {
+        if (payload === 'test') {
+          return { ...state, name: 'hello world' }
+        } else {
+          return { ...state, name: payload }
+        }
+      },
+      setFirstLover: (state, { payload }) => {
+        const { lovers } = state
+        lovers[0] = payload
+        return {
+          ...state,
+          lovers
+        }
+      }
+    }
+  })
+  const store = app.store()
+  store.dispatch({
+    type: 'test/setName',
+    payload: 'test'
+  })
+  store.dispatch({
+    type: 'test/setFirstLover',
+    payload: {
+      name: 'good man',
+      sex: '?'
+    }
+  })
+  const state = store.getState()
+  expect(state.test.name).toBe('hello world')
+  expect(state.test.lovers[0]).toEqual({
+    name: 'good man',
+    sex: '?'
+  })
+})
+
+// 不知道为什么jest跑带有generator的表达式就卡住
+// test('Model with sagas', (done) => {
+//   const s = new Sirius()
+//   s.model({
+//     ...model0,
+//     namespace: 'test',
+//     effects: {
+//       * replaceSecondLover ({ payload }) {
+//         const { put, select, call } = effects
+//         yield put({
+//           type: 'test/setName',
+//           payload: 'bad man'
+//         })
+//         yield call(delay, 200)
+//         const lovers = yield select(state => state.test.lovers)
+//         lovers[1] = payload
+//         yield call(delay, 500)
+//         yield put({
+//           type: 'test/setLovers',
+//           payload: lovers
+//         })
+//       }
+//     }
+//   })
+//   const store = s.store()
+//   store.dispatch({
+//     type: 'test/replaceSecondLover',
+//     payload: {
+//       name: 'bad man',
+//       sex: 10
+//     }
+//   })
+//   const state = store.getState()
+//   setTimeout(() => {
+//     expect(state.test.name).toBe('bad man')
+//     expect(state.test.lovers[1]).toEqual({
+//       name: 'bad man',
+//       sex: 10
+//     })
+//     done()
+//   }, 700)
+// })
