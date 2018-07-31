a4102ed219d7d5e6e0ac3b47e7bf54b6397107b3
Hetian Zhu <zhuht@yunphant.com>
2018-07-31 16:19:08 +0800

    [refactor] use redux-sirius instead of source files

--- examples/redux-count/src/index.js
+++ examples/redux-count/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import Sirius from 'sirius';
+import Sirius from 'redux-sirius';
 import './index.css';
 import App from './Count';
 
