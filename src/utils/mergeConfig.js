19d2742f3da1f42bc76af944677363341fd4a596
Hetian Zhu <zhuht@yunphant.com>
2018-08-24 20:51:24 +0800

    [refactor] Rename function 'merge' to 'mergeObject'

--- src/utils/mergeConfig.js
+++ src/utils/mergeConfig.js
@@ -1,21 +1,22 @@
 export const defaultConfig = {
   models: {},
   middlewares: [],
+  enableThunk: false,
   devtools: {
     enable: true,
     options: {}
   }
 }
-const merge = (origin, replacer) => {
+const mergeObject = (origin, replacer) => {
   return replacer ? { ...origin, ...(replacer || {}) } : origin || {}
 }
 
 export function mergeConfig (config) {
   if (!config) return defaultConfig
   return {
-    models: merge(defaultConfig.models, config.models),
+    models: mergeObject(defaultConfig.models, config.models),
     middlewares: config.middlewares || [],
     // devtools: config.devtools ? config.devtools : defaultConfig.devtools
-    devtools: merge(defaultConfig.devtools, config.devtools)
+    devtools: mergeObject(defaultConfig.devtools, config.devtools)
   }
 }
